91d07681dd0fae3ca8b63a8f03bc14f9854209a0
sanjusjk <sanjusjk@hotmail.com>
2023-01-29 11:00:15 +0530

    updated palindrome.js; formated code

--- palindrome.js
+++ palindrome.js
@@ -1,45 +1,42 @@
-const letter=[]; //This will act as stack
-const letter1=[]; //This will act as stack
+const letter = []; //This will act as stack
+const letter1 = []; //This will act as stack
 
-const input = require('prompt-sync')({sigint: true}); // inorder to make prompt work
+const input = require('prompt-sync')({ sigint: true }); // inorder to make prompt work
 
-let word = input("what is your name? ");//"Bob";
+let word = input("what is your name? ");
 
-let option = input("Choose an option from below?");
+//Option 1
+console.log("firstOption")
+let revWord = word.split('').reverse().join('');
 
 
-
-if(option===1){
-    console.log("firstOption")
-    let revWord = word.split('').reverse().join('');
-
-   
-    if(word.toUpperCase() === revWord.toUpperCase()){
-        console.log(word, " is palindrome");
-        }
-        else{
-            console.log(word, " is not palindrome");
-        }
-    
-}
-else{
-    console.log("second Option")
-    let revWord1;
-    for(let i=0;i<word.length;i++){
-    letter1.push(word[i]);
-    }
-    
-    for(let i=0;i<word.length;i++){
-        revWord1+= letter1.pop(); 
-    }
-    
-    if(word.toUpperCase() === revWord1.toUpperCase()){
+if (word.toUpperCase() === revWord.toUpperCase()) {
     console.log(word, " is palindrome");
-    }
-    else{
-        console.log(word, " is not palindrome");
-    }
 }
+else {
+    console.log(word, " is not palindrome");
+}
+
+
+//Option 2
+
+console.log("second Option")
+let revWord1;
+for (let i = 0; i < word.length; i++) {
+    letter1.push(word[i]);
+}
+
+for (let i = 0; i < word.length; i++) {
+    revWord1 += letter1.pop();
+}
+
+if (word.toUpperCase() === revWord1.toUpperCase()) {
+    console.log(word, " is palindrome");
+}
+else {
+    console.log(word, " is not palindrome");
+}
+
 
 
 
